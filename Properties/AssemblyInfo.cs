203d5da8a4336a782b67da6a165e7502d2b1fad5
TSK <ganr09803@gmail.com>
2026-09-08 21:58:02 +0300

    Align assembly metadata with project version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DashX360")]
-[assembly: AssemblyDescription("DashX360 V1.2.1 public source release")]
+[assembly: AssemblyDescription("DashX360 V1.2.2 public source release")]
 [assembly: AssemblyProduct("DashX360")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
 [assembly: ComVisible(false)]
